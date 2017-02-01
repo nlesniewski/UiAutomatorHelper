495242939494f4b817f4bcf11900c79110b9e0a8
Norbert Leśniewski <norbert.lesniewski@intive.com>
2017-02-01 17:13:04 +0100

    added method to wait for launcher package to appear

--- uiautomatorhelper/src/main/java/it/lesniewski/android/uiautomatorhelper/WaitHelper.java
+++ uiautomatorhelper/src/main/java/it/lesniewski/android/uiautomatorhelper/WaitHelper.java
@@ -4,6 +4,9 @@ import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.Until;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+
 /**
  * Created by nlesniewski on 01.02.2017.
  */
@@ -11,5 +14,11 @@ import android.support.test.uiautomator.Until;
 public class WaitHelper {
     public static void waitForPackageToAppearOnScreen(UiDevice uiDevice, String packageName, int launchTimeout) {
         uiDevice.wait(Until.hasObject(By.pkg(packageName).depth(0)), launchTimeout);
+    }
+
+    public static void waitForLauncherPackageToAppearOnScreen(UiDevice uiDevice, int launchTimeout) {
+        final String launcherPackage = InteractionsHelper.getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        waitForPackageToAppearOnScreen(uiDevice, launcherPackage , launchTimeout);
     }
 }
